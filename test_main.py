889554a250be084ff273b2130fab3b0731662818
agel122 <agel122@yandex.ru>
2022-02-10 22:46:48 +0300

    [008]add tests(work)

--- test_main.py
+++ test_main.py
@@ -5,6 +5,8 @@ from httpx import AsyncClient
 
 import pytest
 
+import jwt
+
 from fastapi.testclient import TestClient
 from .main import app
 from .models import User, User_Pydantic, UserIn_Pydantic, Record, Record_Pydantic, RecordIn_Pydantic
@@ -13,48 +15,35 @@ from tortoise.contrib.test import finalizer, initializer
 
 from passlib.hash import bcrypt
 
+JWT_SECRET = 'myjwtsecret'
+
 
 @pytest.mark.asyncio
-async def test_testpost():
+async def test_usercreate():
+    token = jwt.encode({"id": 1,
+                        "username": "testuser888",
+                        "password_hash": bcrypt.hash("testpass888")}, JWT_SECRET)
     async with AsyncClient(app=app, base_url="http://test") as ac:
         response = await ac.post("/users", json={
             "username": "testuser888",
             "password_hash": bcrypt.hash("testpass888")})
-    assert response.status_code == 200
-    data = response.json()
-    assert data["username"] == "testuser888"
+        assert response.status_code == 200
+        data = response.json()
+        assert data["username"] == "testuser888"
+        assert "id" in data
+        user_id = data["id"]
+        assert user_id == 1
+        response = await ac.post("/records",
+                                 json={"record": "string66"},
+                                 headers={"Authorization": f"Bearer {token}"}
+                                 )
+        assert response.status_code == 200
+        data = response.json()
+        assert data["record"] == "string66"
+        response = await ac.get("/records/my",
+                                headers={"Authorization": f"Bearer {token}"}
+                                )
+        data = response.json()
+        assert data[0]["record"] == "string66"
 
 
-
-
-"""
-@pytest.fixture(scope="module")
-def client() -> Generator:
-    initializer(["models"])
-    with TestClient(app) as c:
-        yield c
-    finalizer()
-
-
-@pytest.fixture(scope="module")
-def event_loop(client: TestClient) -> Generator:
-    yield asyncio.get_event_loop()
-
-
-def test_create_user(client: TestClient, event_loop: asyncio.AbstractEventLoop):
-    response = client.post("/users", json={
-        "username": "testuser888",
-        "password_hash": bcrypt.hash("testpass888")})
-    assert response.status_code == 200, response.text
-    data = response.json()
-    assert data["username"] == "testuser888"
-    assert "id" in data
-    user_id = data["id"]
-
-    async def get_user_by_db():
-        user = await User.get(id=user_id)
-        return user
-
-    user_obj = event_loop.run_until_complete(get_user_by_db())
-    assert user_obj.id == user_id
-"""
